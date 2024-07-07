c330d0c7ec9cac281b3a991644240bf9ce0fde2e
Mbelenga <mbelengaterence@gmail.com>
2024-07-08 00:36:03 +0300

    Updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,19 +7,19 @@ def canUnlockAll(boxes):
     This is a method that determines if all
     the boxes can be opened
     """
-    total = len(boxes)
-    visited = [0]
-    opened = 0
-    current_key = 0
+    unlocked = set([0])
+    keys = [0]
+    len_box = len(boxes)
+    counter = 0
 
-    while current_key < len(visited):
-        current_box = visited[current_key]
+    if len_box == 1:
+        return True
 
-        for key in boxes[current_box]:
-            if 0 < key < total and key not in  visited:
-                visited.append(key)
-                opened += 1
-
-                current_key += 1
-
-            return opened == total - 1
+    while counter < len(keys):
+        index = keys[counter]
+        for key in boxes[index]:
+            if key not in unlocked and key < len_box:
+                unlocked.add(key)
+                keys.append(key)
+        counter += 1
+    return len(unlocked) == len_box
