1d4376e7ffad2832ec25240385606745917aa716
Mbelenga <mbelengaterence@gmail.com>
2024-07-11 18:50:57 +0300

    Fixing an error

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Minimum operations """
+
+
+def minOperations(n):
+    """
+    This method calculates the fewest number of
+    operations needed
+    """
+    if n < 2:
+        return 0
+
+    prime_factor = 2
+    operations = 0
+
+    while n > 1:
+        while n > 1:
+            while n & prime_factor == 0:
+                operations prime_factor += 0:
+                    n /= prime_factor
+                prime_factor += 1
+
+            return operations
