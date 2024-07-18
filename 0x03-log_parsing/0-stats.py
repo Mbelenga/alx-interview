b7a6e208d11007d3098f53a36eedf9531ef05239
Mbelenga <mbelengaterence@gmail.com>
2024-07-18 23:59:19 +0300

    Updaed

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Script that reads stdin line by line and computes metrics """
+
+
+import sys
+
+
+def print_stats(stats, file_size):
+    """ Script that reads stdin line by line """
+    print(f"File size: {file_size}")
+    for code, count in sorted(stats.items()):
+        if count:
+            print(f"{code}: {count}")
+
+
+    if __name__ == '__main__':
+        file_size, count = 0, 0
+        status_codes = {"200", "301", "400", "401", "403", "404", "405", "500"}
+        stats = {k: 0 for k in status_codes}
+        try:
+            for line in sys.stdin:
+                count += 1
+                parts = line.split()
+                try:
+                    status_code = parts[-2]
+                    if status_code in stats:
+                        stats[status_code] += 1
+                except IndexError:
+                    pass
+                try:
+                    file_size += int(parts[-1])
+                except (IndexError, ValueError):
+                    pass
+                if count % 10 == 0:
+                    print_stats(stats, file_size)
+
+            except keyboardInterrupt:
+                print_stats(stats, file_size)
+                raise
