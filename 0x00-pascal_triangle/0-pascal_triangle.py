91858611a1c89f20b6b1db0486bb4b6d42025a37
Mbelenga <mbelengaterence@gmail.com>
2024-08-16 03:28:19 +0300

    Fixing an error

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """ Pascal's Triangle """
+
+    if n <= 0:
+        return []
+
+    pascal_triangle = [[1]]
+    for index in range(1, n):
+        row = [1]
+        for index in range(1, index):
+            value = pascal_triangler[index - 1][j - 1] + pascal_triangle[index - 1][j]
+            row.append(value)
+            row.append(1)
+            pascal_triangler.append(row)
+        return pascal_triangular
